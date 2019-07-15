abd4c0c0c7a87d00c6411f30651893ff6e1ce74d
Rohan Basavaraju <32023234+RohanBasavaraju@users.noreply.github.com>
2019-07-14 21:42:53 -0400

    Update linked_list.py

--- code/data_structures/src/linked_list/linked_list.py
+++ code/data_structures/src/linked_list/linked_list.py
@@ -1,52 +1,66 @@
-''' simple prpgrame for single linked list in python '''
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+    def get_position(self, position):
+        """Get an element from a particular position.
+        Assume the first position is "1".
+        Return "None" if position is not in the list."""
+        counter = 1
+        current = self.head
+        while current:
+            if counter == position:
+                return current
+            else:
+                current = current.next
+                counter += 1
+        return None
+
+    def insert(self, new_element, position):
+        """Insert a new node at the given position.
+        Assume the first position is "1".
+        Inserting at position 3 means between
+        the 2nd and 3rd elements."""
+        counter = 1
+        current = self.head
+        if position == 1:
+            new_element.next = self.head.next
+            self.head = new_element
+        else:
+            while current:
+                if counter == position-1:
+                    new_element.next = current.next
+                    current.next = new_element
+                    break
+                else:
+                    current = current.next
+                    counter += 1
 
 
-Head=None   ''' head node, initilase Null or none '''
-Tail=None   ''' tail node, initilase Null or none '''
-
-'''  object class '''
-
-class A:
-	def __init__(self):
-		self.data=0
-		self.next=None
-
-''' function for creating node '''
-
-def create_node():
-	t=A()
-	return t
-
-'''  function for inserting node '''
-
-def insert_node():
-	global Head,Tail
-	temp=create_node()
-	print("Enter your value",end="\n")
-	a=input()	
-	temp.data=a
-	if(Head==None):
-		Head=temp
-		Tail=temp
-	else:
-		Tail.next=temp
-		Tail=temp
-
-''' function for traversing linked list '''
-
-def traverse(Head):
-	t=Head
-	while(t.next!=None):
-		print(t.data,end="\n")
-		t=t.next
-	print(t.data,end="\n")
-
-''' main function '''
-
-insert_node()
-insert_node()
-insert_node()
-insert_node()
-
-traverse(Head)   ''' passing head node for tarversing'''
+    def delete(self, value):
+        """Delete the first node with a given value."""
+        current = self.head
+        if current.value == value:
+            self.head = current.next
+        else:
+            while current.next:
+                if current.next.value == value:
+                    current.next = current.next.next
+                    break
+                else:
+                    current = current.next
 
